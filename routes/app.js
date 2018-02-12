05f9e53e7e6df743b06d1f8db29316659cba3c62
CR <rajshekhar@protonmail.com>
2018-02-12 00:53:01 -0600

    mongoDB and mongoose setup..test by storing and fetching data from DB

--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    User.findOne({},function(err, doc){
+        if(err){
+            return res.send('Error!');
+        }
+res.render('node', {email: doc});
+
+    }); 
 });
 
-router.get('/message/:msg', function (req, res, next) {
-        res.render('node', {message: req.params.msg});
-    });
+/* router.get('/message', function (req, res, next) {
+        res.render('node');
+    }); */
 
-    router.post('/message', function(req,res,next){
-        var message = req.body.message;
-        res.redirect('/message/' + message);
+    router.post('/', function(req,res,next){
+        var email = req.body.email;
+        var user = new User({
+            firstName: 'Raja',
+            lastName: 'Challapu',
+            password: 'raja',
+            email: email
+
+        });
+
+        //save data in DB
+        user.save();
+        res.redirect('/');
 
     });
 
