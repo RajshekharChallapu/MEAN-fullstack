19baac1435e1e81e669d4f87d22c21d2d45cddac
CR <rajshekhar@protonmail.com>
2018-02-20 16:19:47 -0600

    Angulr 2 UI creation

---
+++ assets/app/auth/user.model.ts
@@ -0,0 +1,7 @@
+export class User {
+
+    constructor(public emial : string, 
+                public password : string, 
+                public firstName?: string, 
+                public lastName?: string) {}
+}
--- routes/app.js
+++ routes/app.js
@@ -1,39 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
+/* var User = require('../models/user'); */
 
 router.get('/', function (req, res, next) {
-    var email ='';
-    User.findOne({},function(err, doc){
-        if(err){
-            return res.send('Error!');
-        }
-        if(doc){
-            email=doc.email;
-        }
-res.render('node',{email: email});
 
-    }); 
+res.render('index');
+    
 });
-
-/* router.get('/message', function (req, res, next) {
-        res.render('node');
-    }); */
-
-    router.post('/', function(req,res,next){
-        var email = req.body.email;
-        var user = new User({
-            firstName: 'Raja',
-            lastName: 'Challapu',
-            password: 'raja',
-            email: email
-
-        });
-
-        //save data in DB
-        user.save();
-        res.redirect('/');
-
-    });
-
 module.exports = router;
