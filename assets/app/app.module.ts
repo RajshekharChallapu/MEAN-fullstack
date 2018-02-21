609a1702586f0f6a84344591bdf9c72dd10a5e6d
CR <rajshekhar@protonmail.com>
2018-02-20 23:13:14 -0600

    directives

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from "./app.component";
 import { MessageComponent } from './messages/message.component';
 
 @NgModule({
+/* the view classes that belong to this module.Angular has three kinds of view classes : components,
+directives,
+and pipes. */
     declarations: [
         AppComponent,
         MessageComponent
     ],
+/* other modules whose exported classes are needed by component templates declared in this module. */
     imports: [BrowserModule, FormsModule],
+    /* bootstrap(main application view) is root component..only root module set this bootstrap property */
     bootstrap: [AppComponent]
 })
+
 export class AppModule {
 
 }
