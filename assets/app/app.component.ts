e044e91ce5be3df73bdb8ce3991cd82ab6bca46c
CR <rajshekhar@protonmail.com>
2018-02-22 20:26:55 -0600

    Adding  Input  and List Componnets..save content message.

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import {Message} from "./messages/message.model";
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    messages: Message [] = [
-        new Message('Some message','Raja'),
-        new Message('Something else','Raja'),
-        new Message('3rd message', 'Raja')
-    ];
+   
     
 }
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { MessageComponent } from './messages/message.component';
+import { MessageListComponent } from './messages/message-list.component';
+import { MessageInputComponent } from './messages/message-input.component';
 
 @NgModule({
 /* the view classes that belong to this module.Angular has three kinds of view classes : components,
@@ -11,7 +13,9 @@ directives,
 and pipes. */
     declarations: [
         AppComponent,
-        MessageComponent
+        MessageComponent,
+        MessageListComponent,
+        MessageInputComponent
     ],
 /* other modules whose exported classes are needed by component templates declared in this module. */
     imports: [BrowserModule, FormsModule],
