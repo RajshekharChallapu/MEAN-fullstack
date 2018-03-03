44bc02c968d76e9f14e5a3f850a67b4b96d4ca94
CR <rajshekhar@protonmail.com>
2018-03-03 13:15:03 -0600

    User signup with reactive data driven form

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 import { AppComponent } from "./app.component";
 import { MessageComponent } from "./messages/message.component";
 import { MessageListComponent } from "./messages/message-list.component";
@@ -30,7 +31,7 @@ and pipes. */
         SigninComponent
     ],
 /* other modules whose exported classes are needed by component templates declared in this module. */
-    imports: [BrowserModule, FormsModule,routing],
+    imports: [BrowserModule, FormsModule,routing, ReactiveFormsModule],
     /* bootstrap(main application view) is root component..only root module set this bootstrap property */
     bootstrap: [AppComponent]
 })
