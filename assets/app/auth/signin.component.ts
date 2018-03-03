02dce2a45c7e258babaffa12bff2c4dc3a6c6fc8
CR <rajshekhar@protonmail.com>
2018-03-03 13:35:18 -0600

    sign in and messges delete component finished

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,9 +1,29 @@
 import {Component} from "@angular/core";
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-signin',
     templateUrl:'./signin.component.html'
 })
 export class SigninComponent {
+myForm : FormGroup;
+onSubmit() {
+    console.log(this.myForm);
+    this
+        .myForm
+        .reset();
+}
+
+ngOnInit() {
+    this.myForm = new FormGroup({
+       /*  firstName: new FormControl(null, Validators.required),
+        lastName: new FormControl(null, Validators.required), */
+        email: new FormControl(null, [
+            Validators.required, Validators.pattern("/^([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)@([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)[\\.]" +
+                    "([a-zA-Z]{2,9})$/")
+        ]),
+        password: new FormControl(null, Validators.required)
+    });
+}
 
 }
