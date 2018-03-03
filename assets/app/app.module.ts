5c44132b066cebc891c57b0cefca8e5408eac429
CR <rajshekhar@protonmail.com>
2018-03-02 23:13:39 -0600

    setting up routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -9,6 +9,7 @@ import { MessageInputComponent } from './messages/message-input.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthenticationComponent } from './auth/authentication.component';
 import { HeaderComponent } from './header.component';
+import { routing } from "./app.routing";
 
 @NgModule({
 /* the view classes that belong to this module.Angular has three kinds of view classes : components,
@@ -24,7 +25,7 @@ and pipes. */
         HeaderComponent
     ],
 /* other modules whose exported classes are needed by component templates declared in this module. */
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule,routing],
     /* bootstrap(main application view) is root component..only root module set this bootstrap property */
     bootstrap: [AppComponent]
 })
--- assets/app/messages/messages.component.ts
+++ assets/app/messages/messages.component.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
     selector: 'app-messages',
     template: `
 <div class = "row"> 
-<app-message-input></app-message-input> < /div>
+<app-message-input></app-message-input> </div>
     <hr>
     <div class="row">
         <app-message-list>
