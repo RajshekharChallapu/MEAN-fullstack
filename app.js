c3db209f9086d13c9932bfa9ea8d10370e27207e
CR <rajshekhar@protonmail.com>
2018-03-03 20:05:59 -0600

    Backend saving messages

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-
 var appRoutes = require('./routes/app');
+var messageRoutes = require('./routes/messages');
+
 var app = express();
 mongoose.connect('mongodb://localhost:27017/rajaDB');
 
@@ -32,6 +33,7 @@ app.use(function (req, res, next) {
     next();
 });
 
+app.use('/message', messageRoutes);
 app.use('/', appRoutes);
 
 // catch 404 and forward to error handler
--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -16,7 +16,7 @@ export class SignupComponent implements OnInit{
      this.myForm = new FormGroup({
        firstName: new FormControl(null, Validators.required), 
        lastName: new FormControl(null, Validators.required), 
-        email: new FormControl(null,[
+       email: new FormControl(null,[
             Validators.required,
             Validators.pattern("/^([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)@([0-9a-zA-Z]([-_\\.]*[0-9a-zA-Z]+)*)[\\.]" +
         "([a-zA-Z]{2,9})$/")
